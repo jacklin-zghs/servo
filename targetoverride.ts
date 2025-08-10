9c221560691e5684b48d23a1ff4774153b406b63
jacklin-zghs <jacklin@tmail.hc.edu.tw>
2025-08-10 13:49:00 +0800

    Update targetoverride.ts

--- targetoverride.ts
+++ targetoverride.ts
@@ -7,8 +7,22 @@ namespace servos {
     export const P2 = new servos.PinServo(pins.P2);
    //% block="servo P8" fixedInstance whenUsed
     export const P8 = new servos.PinServo(pins.P8);
-  //% block="servo P12" fixedInstance whenUsed
-    export const P12 = new servos.PinServo(pins.P12);
   //% block="servo P16" fixedInstance whenUsed
     export const P16 = new servos.PinServo(pins.P16);
+
+  // below are unsafe conflict with led control
+  //% block="servo P3x" fixedInstance whenUsed, unsafe
+    export const P3 = new servos.PinServo(pins.P3); 
+  //% block="servo P4x" fixedInstance whenUsed, unsafe
+    export const P4 = new servos.PinServo(pins.P4); 
+  //% block="servo P6x" fixedInstance whenUsed, unsafe
+    export const P6 = new servos.PinServo(pins.P6); 
+  //% block="servo P7x" fixedInstance whenUsed, unsafe
+    export const P7 = new servos.PinServo(pins.P7); 
+  //% block="servo P9x" fixedInstance whenUsed, unsafe
+    export const P9 = new servos.PinServo(pins.P9); 
+  //% block="servo P10x" fixedInstance whenUsed, unsafe
+    export const P10 = new servos.PinServo(pins.P10); 
+  //% block="servo P12x" fixedInstance whenUsed, unsafe
+    export const P12 = new servos.PinServo(pins.P12);   
 }
