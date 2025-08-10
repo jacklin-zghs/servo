a01ce3317c378093f7e77fe396941bb0a8d8905f
jacklin-zghs <jacklin@tmail.hc.edu.tw>
2025-08-10 13:39:17 +0800

    Create targetoverride.ts

---
+++ targetoverride.ts
@@ -0,0 +1,14 @@
+namespace servos {
+    //% block="servo P0" fixedInstance whenUsed
+    export const P0 = new servos.PinServo(pins.P0);
+    //% block="servo P1" fixedInstance whenUsed
+    export const P1 = new servos.PinServo(pins.P1);
+    //% block="servo P2" fixedInstance whenUsed
+    export const P2 = new servos.PinServo(pins.P2);
+   //% block="servo P8" fixedInstance whenUsed
+    export const P8 = new servos.PinServo(pins.P8);
+  //% block="servo P12" fixedInstance whenUsed
+    export const P12 = new servos.PinServo(pins.P12);
+  //% block="servo P16" fixedInstance whenUsed
+    export const P16 = new servos.PinServo(pins.P16);
+}
